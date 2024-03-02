1fac49bfc5ccb2a874c3246f8decd88e72016eed
Trieu Cao <cvntrieu@gmail.com>
2024-03-02 19:00:29 +0700

    6th Commit
    Tạo BaseObject

--- Weekly_Coding_Exercises/Week1.cpp
+++ Weekly_Coding_Exercises/Week1.cpp
@@ -1,1 +1,32 @@
 
+#include <iostream>
+
+using namespace std;
+
+struct Address {
+
+	string street;
+	int number;
+	
+	Address() {}
+
+	Address(string _street, int _number) {
+		street = _street;
+		number = _number;
+	}
+};
+
+
+int main() {
+
+	int x = 7, y = 45, z = 63, num;
+
+	if (x < z) num = x;
+
+	else num = z;
+
+	if (y < z) num = y;
+
+	else num = z;
+	cout << num;
+}
